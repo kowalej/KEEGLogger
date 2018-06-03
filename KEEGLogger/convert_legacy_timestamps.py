ffc5f85893e8b84ec6773e87f197eebf94c8460e
kowalej <kowaleskij@gmail.com>
2018-06-03 12:36:49 -0400

    Fixed timezone for a couple users that was behind by an extra  hour due to daylight savings time when the recordings were done. Previously only shifted these ones 4 hours for EST -> GMT, but really needed 5 hour shift!

--- KEEGLogger/convert_legacy_timestamps.py
+++ KEEGLogger/convert_legacy_timestamps.py
@@ -5,8 +5,8 @@ import pandas as pd
 
 est_to_gmt_shift = 3600*1000*4 # 3600 seconds in an hour, 1000 ms in a second, and we shift 4 hours to get from EST to GMT
 
-def shift_timezone(timestamp):
-    return timestamp + est_to_gmt_shift
+def shift_timezone(timestamp, shift = est_to_gmt_shift):
+    return timestamp + shift
 
 def ms_to_sec(timestamp):
     return timestamp / 1000.
@@ -18,8 +18,8 @@ def ms_to_sec(timestamp):
 #     df['timestamp'] = df['timestamp'].apply(ms_to_sec, 1)
 #     df.to_csv(filename, index=False)
 
-#for filename in glob.iglob('session_data/legacy-eeg-timestamps-utc-is-est/*/*/*_EEG.csv', recursive=True):
-#     print('Fixing timestamps converting: ' + filename)
+#for filename in glob.iglob('session_data/**/*_EEG.csv', recursive=True):
+#     print('fixing timestamps converting: ' + filename)
 
 #     df = pd.read_csv(filename, float_precision='round_trip')
 #     df['timestamp'] = df['timestamp'].apply(shift_timezone, 1)
